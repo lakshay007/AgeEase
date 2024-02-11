ca0c3bbdd8a17335dc0ab8b0c1413ac3711abc3e
Lepnoxic <akashdevulapalli@gmail.com>
2024-02-11 17:07:04 +0530

    small fix

--- frontend/src/routes/help/+page.svelte
+++ frontend/src/routes/help/+page.svelte
@@ -74,25 +74,27 @@
     })
 </script>
 
-<main>
+<main class="h-full w-full flex flex-col">
     <Nav class="rounded-3xl" />
-    <div class="flex flex-col justify-center items-center gap-y-5">
+    <div class="grow flex flex-col justify-center items-center gap-y-5">
         <div class="text-5xl">Companion Care</div>
         <input bind:value={question} type="text" placeholder="Type here" class="input input-bordered input-black w-[25vw]"/>
         <button on:click={handlehelp} class="btn btn-primary w-[25vw] bg-[#2f4159] text-white border-0">Ask</button>
         <button on:click={vc} class="btn btn-primary w-[25vw] bg-[#2f4159] text-white border-0">Voice</button>
     </div>
-    <div class="words flex flex-col justify-center items-center gap-3" contenteditable>
+    <div class="grow words flex flex-col justify-center items-center gap-3" contenteditable>
         <p id="p"></p>
 
-        <div class="m-5 flex flex-row gap-5 justify-center items-center">
+        <div class="flex flex-row justify-center items-center">
             {#if loaded} 
             <div class="w-[35vw] h-[20vh] overflow-y-scroll bg-white rounded-xl">
             <p class="text-lg semibold m-5"> {res.content} </p>
             </div>
             {:else if loaded === false}
+            <div class="flex flex-row gap-3 items-center justify-center">
             <p class="text-lg">Loading</p>
             <span class="loading loading-spinner loading-lg"></span>
+            </div>
             {:else}
             {/if}
         </div>
