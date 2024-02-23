6199f56c9a67369b91e53cf4dfbfc26094d02b72
Lakshay <dmslakshay@gmail.com>
2024-02-23 22:54:11 +0530

    PORT

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.API_URL)
 .then(()=>{
     console.log("connected to mongodb")
-    app.listen(3000, ()=>{
+    app.listen(PORT, ()=>{
         console.log("app is running on port 3000");
     })
 
